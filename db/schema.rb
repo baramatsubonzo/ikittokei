95ca301e433244e491ff84727e0492dd6050c07f
baramatsubonzo <baramatsubonzo@gmail.com>
2018-09-21 19:30:28 +0900

    product-MVCの完成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_21_065249) do
+ActiveRecord::Schema.define(version: 2018_09_21_092458) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2018_09_21_065249) do
     t.datetime "image_updated_at"
     t.integer "user_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.text "note"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
